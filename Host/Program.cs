14da24fd68f9058372a115b73228b4ccabbf1e0f
eswww <dd@e;sd.com>
2023-01-14 18:42:22 +0500

    add links on Wcf Lib Project and Create Host Main Funtion

---
+++ Host/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Host
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var host = new ServiceHost(typeof(TestApp.ServiceChat)))
+            {
+                host.Open();
+                Console.WriteLine("HOST is Start");
+                Console.ReadLine();
+                Console.ReadLine();
+                Console.ReadLine();
+            }
+        }
+    }
+}
