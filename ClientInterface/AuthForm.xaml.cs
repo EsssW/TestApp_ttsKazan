f6c3ea9a31ecccb389983214ec3c5eee0248f031
eswww <dd@e;sd.com>
2023-01-14 18:45:51 +0500

    add BackEnd Logic for Auth window

---
+++ ClientInterface/AuthForm.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ClientInterface
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthForm.xaml
+    /// </summary>
+    public partial class AuthForm : Window
+    {
+        public AuthForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Control ctl in grid.Children)
+            {
+                if (ctl.GetType() == typeof(TextBox))
+                    ((TextBox)ctl).Text = String.Empty;
+            }
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            Registration regWindow = new Registration();
+            regWindow.Show();
+            Hide();
+        }
+    }
+}
