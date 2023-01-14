c89dcfbaee30c76a1c9479421b9d225ae2c64fb9
eswww <dd@e;sd.com>
2023-01-14 18:40:08 +0500

    create User Entity

---
+++ TestApp/ServerUser.cs
@@ -0,0 +1,13 @@
+﻿using System.ServiceModel;
+
+namespace TestApp
+{
+    public class ServerUser
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public OperationContext operationContext { get; set; }
+    }
+}
